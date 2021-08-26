b4d3a89bf17765dee99eae40869ae9c53f8d0061
nelson10 <38512291+nelson10@users.noreply.github.com>
2021-08-26 12:20:25 -0500

    Add files via upload

---
+++ Src/hypersnapshot.m
@@ -0,0 +1,28 @@
+function [T] = hypersnapshot(RGB,M,N,L,shot,Order_fil,nc,G)
+
+T = zeros(M,N,L,shot); % coded aperture for each snapshot
+npd = 16;
+fltlmbd = 5;
+I = RGB(:,:,2);
+I = mat2gray(I);
+[sl, sh] = lowpass(I, fltlmbd, npd);
+sh = mat2gray(sh);
+
+% Compute the thresholds
+thresh = multithresh(sh,nc-1);
+%thresh = linspace(min(I(:)),max(I(:)),nc);
+% Apply the thresholds to obtain segmented image
+quantization = imquantize(sh,thresh);
+%imagesc(quantization)
+%Q = max(unique(quantization(:)));
+
+for i=1:shot
+    for m = 1:M
+        for n=1:N
+            tm = quantization(m,n);
+            tm2 = Order_fil(i,tm);
+            T(m,n,:,i) = G(:,tm2); 
+        end
+    end
+end
+end
